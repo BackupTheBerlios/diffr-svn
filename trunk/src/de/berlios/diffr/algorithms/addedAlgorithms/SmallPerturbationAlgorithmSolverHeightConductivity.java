2011df015b4878a8c561a42603996e3fed4bf367
andrmikheev <andrmikheev@f5931411-e627-0410-8cb3-cb200e20dce1>
2007-06-24 19:17:58 +0000

    small perturbation algorithm modified

--- trunk/src/de/berlios/diffr/algorithms/addedAlgorithms/SmallPerturbationAlgorithmSolverHeightConductivity.java
+++ trunk/src/de/berlios/diffr/algorithms/addedAlgorithms/SmallPerturbationAlgorithmSolverHeightConductivity.java
@@ -48,8 +48,6 @@ public class SmallPerturbationAlgorithmSolverHeightConductivity extends SmallPer
 		for (int m = -f_size; m <= f_size; m++){
 			Complex c_mn = null;
 			if ( polarization == ReflectedPlaneWave.polarizationE ) {
-
-				
 				Complex t1 = gam(m).mul(gam(m)).sub( gam(0).mul(gam(0))  );
 				Complex t2 = t1.add(m*n);
 				Complex t3 = Complex.i.mul(gam(m)).mul(h).add(1.0);
@@ -68,13 +66,26 @@ public class SmallPerturbationAlgorithmSolverHeightConductivity extends SmallPer
 				
 				s = s.add( c_mn.mul(f(m)).mul(f(n-m)) );
 			} else {
-//				mnmnmn
-				s = s.add((f(m).mul(f(n-m)).mul(-2*(gam0sqweared()-lamNull*n)).div(gam(m).mul(gam(n)))).mul(gam0sqweared()-lamNull*m-lam(m)*n)  );  //  ( Complex.i.mul(2.0).mul( gam0sqweared()-lamNull*n ).div(gam(n)) ).mul(f(n));
+				Complex t1 = gam(m).mul(gam(m)).sub( gam(0).mul(gam(0))  );
+				Complex t2 = t1.add(m*n);
+				Complex t3 = Complex.i.mul(gam(m)).mul(h).add(1.0);
+				Complex t4 = h.mul(t2).mul(t3);
+				Complex t5 = gam(0).mul(gam(0)).sub( m*lam(0) );
+				Complex t6 = h.mul(h).mul( t5 ).add(1.0).mul(2.0);
+				Complex t7 = h.mul(lam(m)).mul(n-m);
+				Complex t8 = t3;
+				Complex t9 = Complex.i.mul(gam(m)).mul(t8);
+				Complex t13 = t7.add(t9);
+				Complex t14 = t6.mul(t13);
+				Complex t10 = Complex.i.mul(gam(0)).mul(h).add(1.0);
+				Complex t11 = t3;
+				Complex t12 = Complex.i.mul(gam(n)).mul(h).add(1.0);
+				c_mn = Complex.i.mul(gam(0)).mul(-1.0).mul( t4.add(t14) ).div(t10).div(t11).div(t12);
+				
+				s = s.add( c_mn.mul(f(m)).mul(f(n-m)) );
 			}
 		}
-		if ( polarization == ReflectedPlaneWave.polarizationE ) {
-			s = s.mul(2.0).mul(gam(0));
-		}
+			s = s.mul(-1.0).mul(Complex.i).mul(gam(0));
 		return s;
 	}
 	}
