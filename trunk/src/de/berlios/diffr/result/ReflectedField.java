dd7e69b6160777032967c6369558eb1860fc8101
petr_mikheev <petr_mikheev@f5931411-e627-0410-8cb3-cb200e20dce1>
2007-09-13 20:42:33 +0000

    Petya's refactoring

--- trunk/src/de/berlios/diffr/result/ReflectedField.java
+++ trunk/src/de/berlios/diffr/result/ReflectedField.java
@@ -1,7 +1,32 @@
 package de.berlios.diffr.result;
 
+import Org.netlib.math.complex.Complex;
 import de.berlios.diffr.*;
+import de.berlios.diffr.inputData.ImpingingField;
+import de.berlios.diffr.inputData.InputData;
 
-public abstract class ReflectedField extends Model {
-
+public class ReflectedField extends Model {
+	private ReflectedPlaneWave[] waves;
+	private ImpingingField impingingWave;
+	public ReflectedField(ReflectedPlaneWave[] waves, ImpingingField impingingPlaneWave) {
+		this.impingingWave = impingingPlaneWave;
+		this.waves = waves;
+	}
+	public ReflectedPlaneWave[] getWaves() {
+		return (ReflectedPlaneWave[]) waves.clone();
+	}
+	public ImpingingField getImpingingWave() {
+		return impingingWave;
+	}
+	public ReflectedField dimensioning(InputData inputData) {
+		ReflectedPlaneWave[] dimensionWaves = new ReflectedPlaneWave[waves.length];
+		double period = inputData.getSurface().getShape().getPeriod();
+		Complex impingingWaveAmplitude = inputData.getImpingingField().getAmplitude();
+		for (int j=0; j< waves.length; j++) {
+			dimensionWaves[j] = waves[j].dimensioning(period, impingingWaveAmplitude);
+		}
+		
+		ImpingingField dimensionImpingingWave = inputData.getImpingingField();
+		return new ReflectedField(dimensionWaves, dimensionImpingingWave);
+	}
 }
