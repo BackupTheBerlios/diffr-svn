1c754d7ed87a1ef76e0c01994f9b10a81d3906f4
petr_mikheev <petr_mikheev@f5931411-e627-0410-8cb3-cb200e20dce1>
2007-09-13 20:44:54 +0000

    Petya's refactoring

--- trunk/src/de/berlios/diffr/result/SurfaceCurrent.java
+++ trunk/src/de/berlios/diffr/result/SurfaceCurrent.java
@@ -1,8 +1,30 @@
 package de.berlios.diffr.result;
 
+import Org.netlib.math.complex.Complex;
 import de.berlios.diffr.*;
 import de.berlios.diffr.inputData.InputData;
 
-public abstract class SurfaceCurrent extends Model {
-	public abstract SurfaceCurrent dimensioning(InputData inputData);
+public class SurfaceCurrent extends Model {
+	private Complex[] points;
+	private double surfacePeriod;
+	public SurfaceCurrent(Complex[] points, double period) {
+		this.points = points;
+		this.surfacePeriod = period;
+	}
+	public Complex get(int number) {
+		return points[number];
+	}
+	public int getPointsNumber() {
+		return points.length;
+	}
+	public double getSurfacePeriod() {
+		return surfacePeriod;
+	}
+	public SurfaceCurrent dimensioning(InputData inputData) {
+		for (int j = 0; j < points.length; j++){
+			points[j] = points[j].mul(inputData.getImpingingField().getAmplitude());
+		}
+		surfacePeriod = inputData.getSurface().getShape().getPeriod();
+		return this;
+	}
 }
