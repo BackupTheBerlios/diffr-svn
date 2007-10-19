055991f63470f4f99a5263e742ae6f61b7b7cec4
andrmikheev <andrmikheev@f5931411-e627-0410-8cb3-cb200e20dce1>
2007-10-19 18:57:05 +0000

    new file added

---
+++ trunk/src/de/berlios/diffr/result/SurfaceCurrentViewText.java
@@ -0,0 +1,41 @@
+package de.berlios.diffr.result;
+
+import java.awt.BorderLayout;
+import java.awt.Graphics;
+
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+import Org.netlib.math.complex.Complex;
+
+import de.berlios.diffr.View;
+
+public class SurfaceCurrentViewText extends View {
+	private static final long serialVersionUID = 1L;
+	private JTextArea text = new JTextArea();
+	private JScrollPane scrollText = new JScrollPane(text);
+	private String result = null;
+	private SurfaceCurrent current = null;
+	public SurfaceCurrentViewText(SurfaceCurrent current) {
+		this.current = current;
+	}
+	public void paintComponent(Graphics g) {
+		if (result==null) {
+			result = "";
+			result += "calculate " + current.getPointsNumber() + " points:\n";
+			for (int a = 0; a < current.getPointsNumber(); a++) {
+				Complex c = current.get(a);
+				result += "x: " + a * current.getSurfacePeriod()
+						/ current.getPointsNumber();
+				result += " phase: " + c.arg();
+				result += " abs: " + c.abs() + "\n";
+			}
+			text.setText(result);
+			text.setEditable(false);
+			this.setLayout(new BorderLayout());
+			this.add(scrollText);
+			this.validate();
+		}		
+		super.paintComponent(g);
+	}
+}
