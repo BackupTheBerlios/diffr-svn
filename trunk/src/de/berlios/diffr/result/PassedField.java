8f155ecf02ef08518c3b9545b5c29d586cfd2f82
petr_mikheev <petr_mikheev@f5931411-e627-0410-8cb3-cb200e20dce1>
2007-10-19 18:17:37 +0000

    serial version ID added

--- trunk/src/de/berlios/diffr/result/PassedField.java
+++ trunk/src/de/berlios/diffr/result/PassedField.java
@@ -2,12 +2,10 @@ package de.berlios.diffr.result;
 
 import de.berlios.diffr.Model;
 import de.berlios.diffr.algorithms.DimensionData;
-import de.berlios.diffr.inputData.InputData;
 
 public abstract class PassedField extends Model {
-
+	private static final long serialVersionUID = 1L;
 	public PassedField dimensioning(DimensionData d) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
